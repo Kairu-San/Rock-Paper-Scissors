0e6dac7234fc9c681bca2b24ccba755fdec57354
root <kylelegaspi10@gmail.com>
2026-06-06 16:22:48 +0800

    Add random rps picker

---
+++ script.js
@@ -0,0 +1,12 @@
+// Function to pick rps
+function getComputerChoice() {
+
+  // Pick a random number 1-3
+  let randomNumber = Math.floor(Math.random * 3);
+console.log(randomNumber);
+  // Display Rock
+  // DisplayPaper
+  // Display Scissors
+}
+
+getComputerChoice();
